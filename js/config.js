f63ae750056b0e38da07d46ee115e5f727b8f460
omimo-bit <dwicky@araiagroup.com>
2026-09-19 01:40:33 +0700

    Update API_URL in config.js

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 window.NC_CONFIG = {
-  API_URL: "https://script.google.com/macros/s/AKfycbytBZsYb04O0u5HWg8bD7lLmW4opFgOe7j2eBKI8Cwusr8pmfU_sEYa_7pU98dzwSgg/exec",
+  API_URL: "https://script.google.com/macros/s/AKfycbwap9uGe_H2UUGbuMQMEW02ewqKJlDrhJqgZHCf-q8nzfPLLu-VUdvPfy2dC5K9UVRV/exec",
   DEMO_MODE: true,
   APP_VERSION: "0.4.0"
 };
