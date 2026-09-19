ece75abdf8cab4f776f733b84641250db24fc585
omimo-bit <dwicky@araiagroup.com>
2026-09-19 23:19:10 +0700

    Delete js directory

--- js/config.js
+++
@@ -1,5 +0,0 @@
-window.NC_CONFIG = {
-  API_URL: "https://script.google.com/macros/s/AKfycbwVdEsiVZu9RnO02jyhfpf3Xl_NpwKRmh71h4z7xjQ9x088t_5SYRixhaEUHKWdACON/exec",
-  DEMO_MODE: true,
-  APP_VERSION: "0.4.0"
-};
