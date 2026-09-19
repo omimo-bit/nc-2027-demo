b2cbecf6e6b5a62c5f87e41b3498ab8d54e62c1d
omimo-bit <dwicky@araiagroup.com>
2026-09-19 17:05:33 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 window.NC_CONFIG = {
-  API_URL: "https://script.google.com/macros/s/AKfycbwap9uGe_H2UUGbuMQMEW02ewqKJlDrhJqgZHCf-q8nzfPLLu-VUdvPfy2dC5K9UVRV/exec",
+  API_URL: "https://script.google.com/macros/s/AKfycbwVdEsiVZu9RnO02jyhfpf3Xl_NpwKRmh71h4z7xjQ9x088t_5SYRixhaEUHKWdACON/exec",
   DEMO_MODE: true,
   APP_VERSION: "0.4.0"
 };
